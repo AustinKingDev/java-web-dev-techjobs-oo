edc418bb88e1f7bd1316288374cbc6edd30bfc8c
Austin King <austintriesemail@gmail.com>
2021-02-14 08:52:12 -0600

    actual finished assignment

--- src/Tests/JobTest.java
+++ src/Tests/JobTest.java
@@ -1,2 +1,69 @@
-package Tests;public class JobTest {
+package Tests;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.PositionType;
+import org.launchcode.techjobs_oo.CoreCompetency;
+import org.launchcode.techjobs_oo.Location;
+import org.launchcode.techjobs_oo.Employer;
+import org.launchcode.techjobs_oo.Job;
+
+import static junit.framework.TestCase.*;
+
+public class JobTest {
+
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+        assertNotSame(job2.getId(), job1.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals("Product tester", testJob.getName());
+        assertEquals("ACME", testJob.getEmployer().getValue());
+        assertEquals("Desert", testJob.getLocation().getValue());
+        assertEquals("Quality control", testJob.getPositionType().getValue());
+        assertEquals("Persistence", testJob.getCoreCompetency().getValue());
+
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertNotSame(job1.getId(), job2.getId());
+
+    }
+
+    @Test
+    public void testToString() {
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals(("\n" + "ID: " + job3.getId() + "\n" + "Name: " + job3.getName() + "\n" + "Employer: " + job3.getEmployer().getValue() + "\n" + "Location: " + job3.getLocation().getValue() + "\n" + "Position Type: " + job3.getPositionType().getValue() + "\n" + "Core Competency: " + job3.getCoreCompetency().getValue() + "\n"), job3.toString());
+
+
+    }
+
+    @Test
+    public void testToStringReturnsBlanks() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        char firstSpace = job.toString().charAt(0);
+        char lastSpace = job.toString().charAt(job.toString().length() - 1);
+        assertEquals(firstSpace, lastSpace);
+
+
+
+    }
+    @Test
+    public void testToStringInitializes() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals("Product tester", job.getName());
+        assertEquals("ACME", job.getEmployer().getValue());
+        assertEquals("Desert", job.getLocation().getValue());
+        assertEquals("Quality control", job.getPositionType().getValue());
+        assertEquals("Persistence", job.getCoreCompetency().getValue());
+    }
 }
+
--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs_oo;
 
-public class Job {
+import java.util.Objects;
+
+public class Job extends JobField{
 
     private int id;
     private static int nextId = 1;
@@ -14,10 +16,102 @@ public class Job {
     // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+
+    }
 
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return id == job.id;
+    }
 
-    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
-    //  and id.
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public  String getName(String name) {
+        return name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
+
+    public String toString() {
+        if (this.getId() <= 0) {
+            return "ID: " + "Data not available" + "\n" + "Name: " + this.getName() + "\n" + "Employer: " + this.getEmployer() + "\n" + "Location: " + this.getLocation() + "\n" + "Position Type: " + this.getPositionType().getValue() + "\n" + "Core Competency: " + this.getCoreCompetency() + "\n";
+        }
+        if (this.getName().isEmpty()) {
+            this.name = "Data not available";
+        } else if (this.getEmployer().getValue().isEmpty()) {
+            this.employer.setValue("Data not available");
+        } else if (this.getLocation().getValue().isEmpty()) {
+            this.location.setValue("Data not available");
+        } else if (this.getPositionType().getValue().isEmpty()) {
+            this.positionType.setValue("Data not available");
+        } else if (this.getCoreCompetency().getValue().isEmpty()) {
+            this.coreCompetency.setValue("Data not available");
+        }
+        return "\n" + "ID: " + this.getId() + "\n" + "Name: " + this.getName() + "\n" + "Employer: " + this.getEmployer() + "\n" + "Location: " + this.getLocation() + "\n" + "Position Type: " + this.getPositionType().getValue() + "\n" + "Core Competency: " + this.getCoreCompetency() + "\n";
+
+        // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
+        //  and id.
+    }
 }
